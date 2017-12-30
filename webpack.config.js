86862f668bf3c073ddfa20c3b47c34aa0e1d695e
Tony Tian <tony.tian@adscale.co.nz>
2017-12-31 12:01:23 +1300

    Getting fundation error

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,49 @@
+var webpack = require('webpack');
+
 module.exports = {
-    entry: './app/app.jsx',
-    output: {
-        path: __dirname,
-        filename: './public/bundle.js'
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+      Main: 'app/components/Main.jsx',
+      Nav: 'app/components/Nav.jsx',
+      Weather: 'app/components/Weather.jsx',
+      WeatherForm: 'app/components/WeatherForm.jsx',
+      WeatherMessage: 'app/components/WeatherMessage.jsx',
+      About: 'app/components/About.jsx',
+      Example: 'app/components/Example.jsx',
+      OpenWeatherMap: 'app/api/OpenWeatherMap.jsx'
     },
-    resolve: {
-        root: __dirname,
-        alias: {
-            Main: 'app/components/Main.jsx', 
-            Nav: 'app/components/Nav.jsx',
-            Weather: 'app/components/Weather.jsx',
-            WeatherForm: 'app/components/WeatherForm.jsx',
-            WeatherMessage: 'app/components/WeatherMessage.jsx',
-            Example: 'app/components/Example.jsx',
-            OpenWeatherMap: 'app/api/OpenWeatherMap.jsx',
-            About: 'app/components/About.jsx'
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-0']
         },
-        extensions: ['', '.js', '.jsx']
-    },
-    module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                query: {
-                    presets: ['react', 'es2015']
-                },
-                test: /\.jsx?$/,
-                exclude: /(node_modules|bower_components)/
-            }
-        ]
-    }, 
-    devtool: 'inline-source-map'
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/
+      }
+    ]
+  },
+  devtool: 'cheap-module-eval-source-map'
 };
